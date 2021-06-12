ccde9d6f638c27bb87ef81fec2f379024f5fc99c
darkcatneko <darkcat7126@gmail.com>
2021-06-13 01:58:13 +0800

    giJJJOe

---
+++ Assets/PillerTest.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PillerTest : MonoBehaviour
+{
+    [SerializeField] GameObject bridge;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Spike"))
+        {
+            bridge.SetActive(true);
+        }
+    }
+}
