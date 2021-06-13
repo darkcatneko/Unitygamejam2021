a18db6a3d654c13206dc6a63a7f5173e7f01f357
darkcatneko <darkcat7126@gmail.com>
2021-06-13 19:19:58 +0800

    nearComplete

--- Assets/EffectExamples/FireExplosionEffects/Prefabs/destroy.cs
+++ Assets/EffectExamples/FireExplosionEffects/Prefabs/destroy.cs
@@ -7,7 +7,10 @@ public class destroy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(this.gameObject, 2f);
+        
+            Destroy(this.gameObject, 1.5f);
+        
+        
     }
 
     // Update is called once per frame
