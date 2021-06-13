fa02a01f248f5410db4b2f2d82983f8608fe5e5c
darkcatneko <darkcat7126@gmail.com>
2021-06-13 16:57:02 +0800

    save

---
+++ Assets/crystleDes.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class crystleDes : MonoBehaviour
+{
+    
+    void Start()
+    {
+        float vanish;
+        vanish = Random.Range(1f, 2.5f);
+        Instantiate(Resources.Load<GameObject>("FlamesParticleEffect"),transform.position,transform.rotation);
+        Destroy(this.gameObject, vanish);
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
